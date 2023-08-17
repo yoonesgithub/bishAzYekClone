8b9c65e58693f677a09f3910c832e26a25a9a178
yoones <yooneskhatami9@gmail.com>
2023-08-17 19:09:37 +0330

    paid details complete

--- courses-and-products/paid-details/app.js
+++ courses-and-products/paid-details/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     $(".open-details-menu").click(function(e) {
         console.log($(e.currentTarget).find("span").html());
-        $(e.currentTarget).find("span").html() == "keyboard_double_arrow_up"
-        ? $(e.currentTarget).find("span").html("keyboard_double_arrow_down")
-        : $(e.currentTarget).find("span").html("keyboard_double_arrow_up")
-        $(e.currentTarget).next().first().slideToggle(300);  
+        $(e.currentTarget).find("span").html() == "remove"
+        ? $(e.currentTarget).find("span").html("add")
+        : $(e.currentTarget).find("span").html("remove")
+        $(e.currentTarget).next().first().slideToggle(600);  
     })
 });
